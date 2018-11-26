3e3ccd0aa9811ce9d899e582a06eab02ea967d16
Eduardo Mínguez <e.minguez@gmail.com>
2018-11-26 15:45:08 +0100

    Added user/pass

--- speedtest2mqtt.sh
+++ speedtest2mqtt.sh
@@ -3,11 +3,13 @@ MQTT_HOST=${MQTT_HOST:-localhost}
 MQTT_ID=${MQTT_ID:-k77}
 MQTT_TOPIC=${MQTT_TOPIC:-speedtest/k77}
 MQTT_OPTIONS=${MQTT_OPTIONS:-"-V mqttv311"}
-SPEEDTEST_OPTIONS=${SPEEDTEST_OPTIONS:-"--bytes"}
+MQTT_USER="myuser"
+MQTT_PASS="mypass"
+SPEEDTEST_OPTIONS=${SPEEDTEST_OPTIONS:-"--simple"}
 
 #sends speedtest-cli data as json to MQTT broker
-/usr/bin/speedtest-cli --simple ${SPEEDTEST_OPTIONS} | /usr/bin/perl -pe 's/^(.*): (.*) (.*?)(\/s)?\n/"$1_$3": $2, /m' | cut -d',' -f 1-3 | while read line
+/usr/bin/speedtest-cli ${SPEEDTEST_OPTIONS} | /usr/bin/perl -pe 's/^(.*): (.*) (.*?)(\/s)?\n/"$1_$3": $2, /m' | cut -d',' -f 1-3 | while read line
 do
-# Raw message to MQTT
-  echo "{$line}"  | /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} -l -t ${MQTT_TOPIC} ${MQTT_OPTIONS}
+  # Raw message to MQTT
+  echo "{$line}"  | /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} -l -t ${MQTT_TOPIC} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS}
 done
